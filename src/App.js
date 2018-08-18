5266c02a81671c7cd1d6e77325bdedaa7f0a4285
caseyhald <privateer.acc@gmail.com>
2018-08-18 14:59:17 -0700

    redux

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import NotificationsSwitch from './components/notifications-switch'
+import UserSettings from './components/user-settings'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
+      <div className='App'>
+        <div className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className='App-intro'>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <hr />
+        <NotificationsSwitch />
+        <hr />
+        <UserSettings />
+
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
